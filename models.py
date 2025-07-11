c759ee9fa66187ad5218f09cd40793283713bf2a
SANDRICKSON <sandroqatamadze802@gmail.com>
2025-07-11 18:28:49 +0400

    Update models.py

--- models.py
+++ models.py
@@ -56,3 +56,10 @@ class ContactMessage(db.Model):
     email = db.Column(db.String(120))
     message = db.Column(db.Text)
     timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+
+
+class Character(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    image_url = db.Column(db.String(300), nullable=True)
+    description = db.Column(db.Text, nullable=False)
