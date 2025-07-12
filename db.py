a4009103335c006ac48afed2dd6f3f038593cfa9
SANDRICKSON <sandroqatamadze802@gmail.com>
2025-07-12 18:17:31 +0400

    Update db.py

--- db.py
+++ db.py
@@ -1,8 +1,10 @@
-from models import User
+from models import User, Character,ChapterAudio
 from extensions import app, db
+from werkzeug.security import generate_password_hash
 
 with app.app_context():
-
-
-    new_user = User()
     db.create_all()
+
+    
+
+ 
